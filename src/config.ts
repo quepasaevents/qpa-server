d7feb262080ab69c304f071305a9410acaef8ced
Amit Jakubowicz <a.jakubowicz@travelaudience.com>
2018-04-02 20:23:31 +0200

    Builds and deploys

--- src/config.ts
+++
@@ -1,7 +0,0 @@
-export const domain = 'quepasaalpujarra.com'
-export const mailgun = {
-  apiKey: 'key-64fc7260cecfd4a8d5fb51e97791b330',
-  domain
-}
-export const projectId = 'calendar-189316'
-
--- src/user.ts
+++
@@ -1,6 +0,0 @@
-import {UserKeys} from './types'
-import repository from './repository'
-
-export const getUser = async (keys: UserKeys) => {
-  return await repository.getUser(keys)
-}
