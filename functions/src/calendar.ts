f4683ad6ad66dfdf722cd66bd72cde3762c212c7
Amit Jakubowicz <a.jakubowicz@travelaudience.com>
2018-05-23 08:09:50 +0200

    Get cal list over REST api

--- functions/src/calendar.ts
+++ functions/src/calendar.ts
@@ -1,30 +1,10 @@
 import { google } from 'googleapis'
+import fetch from 'isomorphic-fetch';
 
 export default class Calendar {
-  listEvents = () => {
-    const calendar = google.calendar({version: 'v3'})
-    calendar.events.list({
-      calendarId: 'primary',
-      timeMin: (new Date()).toISOString(),
-      maxResults: 10,
-      singleEvents: true,
-      orderBy: 'startTime',
-    }, (err, result) => {
-      const data = result && result.data
-      if (err) {
-        console.log('The API returned an error: ' + err);
-        return
-      }
-      const events = data.items;
-      if (events.length) {
-        console.log('Upcoming 10 events:');
-        events.map((event, i) => {
-          const start = event.start.dateTime || event.start.date;
-          console.log(`${start} - ${event.summary}`);
-        });
-      } else {
-        console.log('No upcoming events found.');
-      }
-    });
+  listEvents = async () => {
+    const calList = await fetch('https://www.googleapis.com/calendar/v3/users/me/calendarList')
+    console.log('calList', JSON.stringify(calList))
+    return calList
   }
 }
