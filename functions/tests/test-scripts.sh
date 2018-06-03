102c35341ef95016346a6d2a957cae1dcb9001bc
Amit Jakubowicz <a.jakubowicz@travelaudience.com>
2018-06-03 14:01:31 +0200

    Test script is working and can pass authentication for posting events

--- functions/tests/test-scripts.sh
+++ functions/tests/test-scripts.sh
@@ -4,4 +4,4 @@ curl -X POST "https://staging.quepasaalpujarra.com/api/signup" -H "Content-Type:
 # Sign in
 curl -L -v -s "https://staging.quepasaalpujarra.com/api/signin?hash=Os8gknR71loKKutYAZQnFN7XyIQGbiUzzAoOvxRaddRsMHLt&email=jakubowicz.amit@gmail.com" -H "Client-Agent: curl"
 
-curl -X POST "https://staging.quepasaalpujarra.com/api/events" -H "Cookies: __session=OekzPAjhXamEw9lbR4vxdoPmhV4n74FxfJWTRHN1gX2nZzrv"
+curl -X POST "https://staging.quepasaalpujarra.com/api/events" -H "Cookie: __session=OekzPAjhXamEw9lbR4vxdoPmhV4n74FxfJWTRHN1gX2nZzrv"
