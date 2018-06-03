83f605e2c56325970bd0b46e1243a5c008bf4e76
Amit Jakubowicz <a.jakubowicz@travelaudience.com>
2018-06-03 11:21:40 +0200

    Remove joi

--- functions/src/__tests__/dependencies.spec.ts
+++ functions/src/__tests__/dependencies.spec.ts
@@ -1,25 +1,6 @@
-import * as Joi from 'joi'
 import * as cookie from 'cookie'
 
-describe('joi api', () => {
-  it('test the api', () => {
-    const schema = Joi.object().keys({
-      username: Joi.string().alphanum().min(3).max(30).required(),
-      password: Joi.string().regex(/^[a-zA-Z0-9]{3,30}$/),
-      access_token: [Joi.string(), Joi.number()],
-      birthyear: Joi.number().integer().min(1900).max(2013),
-      email: Joi.string().email()
-    }).with('username', 'birthyear').without('password', 'access_token');
-
-
-    const result = Joi.validate({username: 'abc', birthyear: 1994}, schema);
-    expect(result.error).toBeNull()
-
-// You can also pass a callback which will be called synchronously with the validation result.
-    Joi.validate({username: 'abc', birthyear: 1994}, schema, function (err, value) {
-    });  // err === null -> valid
-  })
-
+describe('dependencies api', () => {
   it('cookie', () => {
     expect(cookie.parse('foo=bar; equation=E%3Dmc%5E2').foo).toBe('bar')
   })
