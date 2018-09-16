7ec93333cfbb110409a5bc1bda5f82cf2da3f3ba
Amit Jakubowicz <a.jakubowicz@travelaudience.com>
2018-09-16 20:34:51 +0200

    Add router

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,20 +1,25 @@
 import * as React from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import './App.css';
 import CreateEvent from './CreateEvent';
+import Login from './Login'
 import logo from './logo.svg';
 
 class App extends React.Component {
   public render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <div className="App-intro">
-          <CreateEvent />
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo"/>
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+          <div className="App-intro">
+            <Route path="/login" component={Login}/>
+            <Route path="/post" component={CreateEvent}/>
+          </div>
         </div>
-      </div>
+      </Router>
     );
   }
 }
