335a7b21fc867540310b0939031eb1d0f55d3231
Amit Jakubowicz <a.jakubowicz@travelaudience.com>
2019-08-02 19:16:35 +0200

    Remove client code

--- client/SSR/index.ts
+++
@@ -1,9 +0,0 @@
-import * as express from 'express'
-import { httpSSRHandler } from './handler'
-
-const port = 5000
-console.log(`Starting SSR on port ${port}...`)
-const app = express()
-app.get('/*', httpSSRHandler)
-app.listen(port)
-console.log('Successfully started')
--- src/Auth/authResolvers.ts
+++ src/Auth/authResolvers.ts
@@ -1,7 +1,7 @@
 import SessionManager, { SessionAlreadyValidatedError } from "./SessionManager"
 import { User } from "./User.entity"
 import { PostOffice } from "../post_office"
-import {GQL} from "../../../@types"
+import {GQL} from "../../@types"
 import {Context} from "../@types/graphql-utils"
 
 interface Dependencies {
